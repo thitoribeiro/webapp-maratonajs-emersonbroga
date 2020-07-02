eb95aa4a655b5bdc7b9d067ad7479f8fe90d7b09
thitoribeiro <thito.ribeiro@hotmail.com>
2020-07-01 22:49:09 -0300

    Configuring router, auth and nodemon

---
+++ links-backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const authController = require('./controllers/auth');
+
+const app = express();
+
+app.use('/auth', authController);
+
+app.get('/', (req, res) => {
+  return res.json('Api running...');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
